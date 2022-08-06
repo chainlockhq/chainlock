17059857bc21b2e18fb74ddf2766f5153e1a434b
Bruno Roemers <bruno@roemers.io>
2022-08-06 10:51:46 +0200

    Show error to user when transaction fails

--- frontend/src/components/first-vault/FirstVaultController.tsx
+++ frontend/src/components/first-vault/FirstVaultController.tsx
@@ -4,6 +4,7 @@ import getVaultFactoryContract from "../../utils/contracts/getVaultFactoryContra
 import lock from "../../utils/hooks/lock"
 import getTransactionUrl from "../../utils/urls/get-transaction-url"
 import FirstVaultCreated from "./FirstVaultCreated"
+import FirstVaultCreationError from "./FirstVaultCreationError"
 import FirstVaultInProgress from "./FirstVaultInProgress"
 import FirstVaultLanding from "./FirstVaultLanding"
 
@@ -15,6 +16,7 @@ interface Props {
 const FirstVaultController = ({ wallet, connectedAddress }: Props) => {
   const [creationInProgress, setCreationInProgress] = useState(false)
   const [createdVaultAddress, setCreatedVaultAddress] = useState<string>()
+  const [creationFailed, setCreationFailed] = useState(false)
 
   const createVault = () => {
     lock(setCreationInProgress, async () => {
@@ -26,6 +28,8 @@ const FirstVaultController = ({ wallet, connectedAddress }: Props) => {
       // TODO compare with process.env.REACT_APP_CHAIN_ID
       // TODO ask to switch chains? https://soliditytips.com/articles/detect-switch-chain-metamask/
 
+      // TODO handle situation where eth node is not available
+
       const signer = provider.getSigner()
       const vaultFactoryContract = getVaultFactoryContract()
 
@@ -35,6 +39,15 @@ const FirstVaultController = ({ wallet, connectedAddress }: Props) => {
       const receipt = await tx.wait()
       console.debug('transaction status:', receipt.status)
 
+      // TODO necessary?
+      // status 1 means transaction successful, see: https://docs.ethers.io/v5/api/providers/types/#providers-TransactionResponse
+      if (receipt.status !== 1) {
+        setCreationFailed(true)
+        return
+      }
+
+      // TODO handle situation where vault factory contract does NOT exist - seems to still return receipt.status = 1
+
       // TODO below may fail => wrong contract / contract not deployed?
       const [vaultAddress] = receipt.events[0].args
 
@@ -43,11 +56,19 @@ const FirstVaultController = ({ wallet, connectedAddress }: Props) => {
       console.debug(`view transaction:`, getTransactionUrl(tx.hash))
 
       setCreatedVaultAddress(vaultAddress)
+    }).catch(e => {
+      console.error('failed to create vault:', e)
+      setCreationFailed(true)
     })
   }
 
   if (creationInProgress) {
     return <FirstVaultInProgress/>
+  }
+
+  if (creationFailed) {
+    return <FirstVaultCreationError/>
+    // TODO button so we can go back to "FirstVaultLanding"
   }
 
   if (!createdVaultAddress) {

3eebebdf12339c22e671a4f7aeba3f898e1e70e3
danisev7 <anievil@gmail.com>
2022-08-09 10:38:29 +0200

    add basic dashboard ui

--- frontend/src/components/dashboard/DashboardController.tsx
+++ frontend/src/components/dashboard/DashboardController.tsx
@@ -1,5 +1,8 @@
 import Wallet from "../../objects/Wallet.interface"
 import Header from "../_shared/molecules/Header"
+import DashboardSecrets from "./DashboardSecrets"
+import SecretType from "./SecretType"
+import { useEffect, useState } from "react"
 
 interface Props {
   wallet: Wallet
@@ -11,8 +14,27 @@ interface Props {
 }
 
 const DashboardController = ({
-  wallet, address, vaultAddress, vaultKeyPair, goToVaultSelect, disconnectWallet
+  wallet,
+  address,
+  vaultAddress,
+  vaultKeyPair,
+  goToVaultSelect,
+  disconnectWallet,
 }: Props) => {
+  const [secrets, setSecrets] = useState<SecretType[]>([])
+  useEffect(() => {
+    if (wallet && address && vaultAddress && vaultKeyPair && secrets.length === 0) {
+      // TODO: Call to the vault contract and read the secrets for current user
+      // and delete this dummy secrets
+      const dummySecrets: SecretType[] = []
+      const secret1: SecretType = { identifier: "Twitter", userName: "Larry", encryptedPassword: "*********" }
+      const secret2: SecretType = { identifier: "Instagram", userName: "John12345", encryptedPassword: "*********" }
+      const secret3: SecretType = { identifier: "Whale Wallet", userName: "Vitalik", encryptedPassword: "*********" }
+      dummySecrets.push(secret1, secret2, secret3)
+      setSecrets(dummySecrets)
+    }
+  }, [wallet, address, vaultAddress, secrets, vaultKeyPair])
+
   return (
     <div>
       <Header
@@ -21,9 +43,7 @@ const DashboardController = ({
         vaultAddress={vaultAddress}
         onChangeVault={goToVaultSelect}
       />
-      <div className="bg-[#2b3f4a] h-screen p-20">
-        <p className="text-3xl font-bold text-white">Your vaults</p>
-      </div>
+      <DashboardSecrets walletAddress={address} vaultAddress={vaultAddress} secrets={secrets} />
     </div>
   )
 }
---
+++ frontend/src/components/dashboard/DashboardSecretsTable.tsx
@@ -0,0 +1,44 @@
+import DashboardSecretsRow from "./DashboardSecretsRow"
+import SecretType from "./SecretType"
+
+interface Props {
+  secrets: SecretType[]
+}
+
+const DashboardSecretsTable = ({ secrets }: Props) => {
+  return (
+    <div className="flex flex-col">
+      <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="py-4 inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="overflow-hidden">
+            <table className="min-w-full text-center">
+              <thead className="border-b bg-gray-800">
+                <tr>
+                  <th scope="col" className="text-sm font-medium text-white px-6 py-4">
+                    #
+                  </th>
+                  <th scope="col" className="text-sm font-medium text-white px-6 py-4">
+                    Identifier
+                  </th>
+                  <th scope="col" className="text-sm font-medium text-white px-6 py-4">
+                    UserName
+                  </th>
+                  <th scope="col" className="text-sm font-medium text-white px-6 py-4">
+                    Password
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {secrets.map((secret, index) => {
+                  return <DashboardSecretsRow index={index} secret={secret} />
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default DashboardSecretsTable
